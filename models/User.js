f395a427519a869275d58012186c95c24f8179ee
dayoadisa <dayoadisa@gmail.com>
2019-11-18 13:35:05 +0200

    login functionality added

--- models/User.js
+++ models/User.js
@@ -30,6 +30,22 @@ User.prototype.validate = function() {
     if (this.data.username.length > 30) {this.errors.push('username cannot exceed 30')}
 }
 
+User.prototype.login = function() {
+    return new Promise((resolve, reject) => {
+        this.cleanUp()
+        usersCollection.findOne({username: this.data.username}).then((attemptedUser) => {
+            if(attemptedUser && attemptedUser.password == this.data.password) {
+                resolve("congrats!!!")
+                } else {
+                reject("invalid username or password")
+                    }
+        }).catch(function() {
+            reject("Please try again later")
+        })
+    })
+
+}
+
 User.prototype.register = function() {
     //validate user data
     this.cleanUp()
